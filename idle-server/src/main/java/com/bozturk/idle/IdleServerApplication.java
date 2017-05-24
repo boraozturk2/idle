dd7cee066ee17b8a106d9bb25ff1f118181d0848
boraozturk@hotmail.com <boraozturk@hotmail.com>
2017-05-24 11:44:35 +0300

    zfzxcxzc

---
+++ idle-server/src/main/java/com/bozturk/idle/IdleServerApplication.java
@@ -0,0 +1,12 @@
+package com.bozturk.idle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdleServerApplication.class, args);
+	}
+}
